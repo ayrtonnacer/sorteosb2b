9aef2f5de23ed88ca359edee24f6fd58a6a50cc4
Ayrton Nacer <hiayrtonnacer@gmail.com>
2026-02-09 15:47:45 -0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/sorteosb2b/',
   server: {
     host: "::",
     port: 8080,
